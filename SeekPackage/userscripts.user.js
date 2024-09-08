014383ed5611edf8abe31ac24e60cb47fa33e4bc
Seek Cat <seekcat033@gmail.com>
2024-09-08 22:10:19 +0700

    Create userscripts.user.js

---
+++ SeekPackage/userscripts.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Bypass Keysystem
+// @homepageURL  https://discord.gg/ZHPXxnArga
+// @version      2.5
+// @description  Bypass Platoboost System
+// @author       SeekCat
+// Keysystem \\
+// @match        https://gateway.platoboost.com/a/*
+// @match        https://seekcat0.github.io/fluxusfake?HWID=*
+
+// Loot-Link \\
+// @match        https://loot-link.com/s?*
+// @match        https://loot-links.com/s?*
+// @match        https://lootlink.org/s?*
+// @match        https://lootlinks.co/s?*
+// @match        https://lootdest.info/s?*
+// @match        https://lootdest.org/s?*
+// @match        https://lootdest.com/s?*
+// @match        https://links-loot.com/s?*
+// @match        https://linksloot.net/s?*
+
+// Linkvertise \\
+// @match        https://linkvertise.com/*/*/dynamic*
+// @match        https://file-link.net/*/*/dynamic*
+// @match        https://link-to.net/*/*/dynamic*
+// @match        https://up-to-down.net/*/*/dynamic*
+// @match        https://direct-link.net/*/*/dynamic*
+
+// Hcaptcha - Recaptcha \\
+// @match        https://*/recaptcha/*
+// @match        https://*.hcaptcha.com/*hcaptcha-challenge*
+// @match        https://*.hcaptcha.com/*checkbox*
+// @match        https://*.hcaptcha.com/*captcha*
+
+// Runtime \\
+
+// @run-at       document-end
+// @run-at       document-start
+
+// Give Premission \\
+// @grant        GM_xmlhttpRequest
+// @grant        GM_notification
+// @grant        GM_openInTab
+
+// Api keysystem - Api Whitelist
+// @connect      api-gateway.platoboost.com
+
+// Home Package \\
+// @icon         https://seekcat0.github.io/seekcat.jpg
+// @require      https://raw.githubusercontent.com/seekcat0/SeekFolder/main/SeekPackage/home.js
+
+// @downloadURL https://raw.githubusercontent.com/seekcat0/SeekFolder/main/SeekPackage/userscripts.user.js
+// @updateURL https://raw.githubusercontent.com/seekcat0/SeekFolder/main/SeekPackage/userscripts.user.js
+// ==/UserScript==
